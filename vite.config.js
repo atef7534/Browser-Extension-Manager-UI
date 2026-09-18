bd0f1e102d1b112f88f1496d1d43cfa7a4def8b8
atif_7534 <atefyasser97@gmail.com>
2026-09-18 16:58:18 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   // Add this line exactly:
-  base: '/Browswer-Extension-Manager-UI/',
+  base: '/Browser-Extension-Manager-UI/',
 })
